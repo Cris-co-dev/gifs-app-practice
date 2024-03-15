b0b03d4063f1d87d05230a244befcf1e57ed7d45
Cris <corsamps@gmail.com>
2024-03-15 07:51:33 -0500

    Update sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { GifsModule } from './gifs/gifs.module';
@@ -15,7 +16,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     GifsModule,
-    SharedModule
+    SharedModule,
+    //* Http requests
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
